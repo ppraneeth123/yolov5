0c406d2c2b12097e70502cad28669c7881ddcb54
ppraneeth123 <74655754+ppraneeth123@users.noreply.github.com>
2022-05-09 22:48:47 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -175,7 +175,7 @@ def run(
                         c = int(cls)  # integer class
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         annotator.box_label(xyxy, label, color=colors(c, True))
-                        annotator.text(xy, text, txt_color=(255, 255, 255), pil=True)
+                        annotator.text(xyxy, s, txt_color=(255, 255, 255), pil=True)
                         if save_crop:
                             save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
                 #cv2.putText(im0, f'Detected objects: {a}', (10, 40), cv2.LINE_AA, 1, colors(c, True), 2)
